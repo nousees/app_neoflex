11e6c5fd551e24aa05cb8140441e5a49677262da
Danila <danil01062004@mail.ru>
2025-04-25 14:39:42 +0400

    hotfix web/index.html

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"main.dart.js": "748164db65fbad183211fcb5f1f3f49a",
-"index.html": "3719bf7194a959f611857b05575604c4",
-"/": "3719bf7194a959f611857b05575604c4",
+"index.html": "5dfe63a8d2f8d9dea90fc9b667bc13d8",
+"/": "5dfe63a8d2f8d9dea90fc9b667bc13d8",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -58,7 +58,7 @@ const RESOURCES = {"main.dart.js": "748164db65fbad183211fcb5f1f3f49a",
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "version.json": "d20caf3174a26ae63ba4b9fe94bab789",
-"flutter_bootstrap.js": "ad368767c5acb72aacb738cf9a9ab465",
+"flutter_bootstrap.js": "7123983e6b7ca11fa764c1481604a049",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796"};
 // The application shell files that are downloaded before a service worker can
 // start.
